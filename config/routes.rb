502399188a5761d9fa4e7f4a5cc053d90edf23dd
vagrant <vagrant@localhost.localdomain>
2022-01-29 11:43:46 +0000

    Finish login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'statice_pages#home'
   get 'statice_pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
